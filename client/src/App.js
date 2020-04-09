72b3799b75caf960083f1181c1252b3d3c1419a0
Yaniv Ziperfal <ziperfal123@gmail.com>
2020-04-09 11:18:51 +0300

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,20 @@ import Fib from './Fib';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib calculator!</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Fib Calculator version 2</h1>
+              <Link to="/">Home</Link>
+              <Link to="/otherpage">Other Page</Link>
+            </header>
+            <div>
+              <Route exact path="/" component={Fib} />
+              <Route path="/otherpage" component={OtherPage} />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
     );
   }
 }
